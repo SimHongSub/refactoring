f66b6ff8936e505ca9b25240606dc24f723eae8a
SimHongSub <ghdtjq2038@gmail.com>
2020-12-24 00:39:22 +0900

    [first_exam] total amount calculation functionalization

--- example/firstRefactoringSource.js
+++ example/firstRefactoringSource.js
@@ -25,15 +25,13 @@ invoices = [
 ];
 
 function statement(invoice, plays){
-    let totalAmount = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for(let perf of invoice.performances){
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${usd(totalAmount)}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 
     return result;
@@ -92,6 +90,17 @@ function statement(invoice, plays){
 
         for(let perf of invoice.performances) {
             result += volumeCreditsFor(perf);
+        }
+
+        return result;
+    }
+
+    // totalAmount 계산 함수화
+    function totalAmount(){
+        let result = 0;
+
+        for(let perf of invoice.performances){
+            result += amountFor(perf);
         }
 
         return result;
