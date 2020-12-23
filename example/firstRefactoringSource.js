66de651097a9515efb4ececd1d6a036de4379242
SimHongSub <ghdtjq2038@gmail.com>
2020-12-24 00:27:42 +0900

    [first_exam] modify format function name

--- example/firstRefactoringSource.js
+++ example/firstRefactoringSource.js
@@ -32,11 +32,11 @@ function statement(invoice, plays){
     for(let perf of invoice.performances){
         volumeCredits += volumeCreditsFor(perf);
 
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${format(totalAmount/100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
 
     return result;
@@ -83,10 +83,10 @@ function statement(invoice, plays){
     }
 
     // format 함수화
-    function format(aNumber){
+    function usd(aNumber){
         return new Intl.NumberFormat("en-US", {
             style: "currency", currency: "USD", minimumFractionDigits: 2
-        }).format(aNumber);
+        }).format(aNumber/100);
     }
 }
 
