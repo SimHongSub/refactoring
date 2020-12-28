ea1944c9d5394ad2649e6436555386ed70951766
SimHongSub <ghdtjq2038@gmail.com>
2020-12-28 17:45:05 +0900

    [first_exam] move volumeCredits function to class

--- example/firstRefactoringSouces/createStatementData.js
+++ example/firstRefactoringSouces/createStatementData.js
@@ -29,6 +29,17 @@ class PerformanceCalculator {
 
         return result;
     }
+
+    // volumeCredits 계산 함수화
+    get volumeCredits(){
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+
+        if("comedy" === this.play.type)
+            result += Math.floor(this.performance.audience / 5);
+
+        return result;
+    }
 }
 
 // statementdata 생성 함수화
@@ -47,7 +58,7 @@ export default function createStatementData(invoice, plays){
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
-        result.volumeCredits = volumeCreditsFor(result);
+        result.volumeCredits = calculator.volumeCredits;
 
         return result;
     }
