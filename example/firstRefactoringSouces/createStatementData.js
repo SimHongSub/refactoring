c1b51ab2e85a834f3e15f22a7b98f0eeb1822a56
SimHongSub <ghdtjq2038@gmail.com>
2020-12-28 17:41:38 +0900

    [first_exam] create calculator class for paerformance

--- example/firstRefactoringSouces/createStatementData.js
+++ example/firstRefactoringSouces/createStatementData.js
@@ -1,3 +1,36 @@
+// 공연료 계산기 클래스
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay
+    }
+
+    // switch문 함수화
+    get amount(){
+        let result = 0;
+
+        switch (this.play.type){
+            case "tragedy":
+                result = 40000;
+                if(this.performance.audience > 30){
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if(this.performance.audience > 20){
+                    result += 10000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${this.play.type}`);
+        }
+
+        return result;
+    }
+}
+
 // statementdata 생성 함수화
 export default function createStatementData(invoice, plays){
     const statementData = {};
@@ -10,9 +43,10 @@ export default function createStatementData(invoice, plays){
 
     // 새로운 중간 데이터 형태를 만들기 위한 함수
     function enrichPerformance(aPerformance){
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
 
         return result;
@@ -21,31 +55,6 @@ export default function createStatementData(invoice, plays){
     // play 변수 함수
     function playFor(aPerformance){
         return plays[aPerformance.playID];
-    }
-
-    // switch문 함수화
-    function amountFor(aPerformance){
-        let result = 0;
-
-        switch (aPerformance.play.type){
-            case "tragedy":
-                result = 40000;
-                if(aPerformance.audience > 30){
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if(aPerformance.audience > 20){
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-        }
-
-        return result;
     }
 
     // volumeCredits 계산 함수화
