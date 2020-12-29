98af79e733c8b606f5bf4c97a1e2d3db8854dcf7
SimHongSub <ghdtjq2038@gmail.com>
2020-12-29 18:57:13 +0900

    [test_code] add province class

--- example/testSampleSource.js
+++ example/testSampleSource.js
@@ -1,3 +1,51 @@
+class Province {
+    constructor(doc) {
+        this._name = doc.name;
+        this._producers = [];
+        this._totalProduction = 0;
+        this._demand = doc.demand;
+        this._price = doc.price;
+        doc.producers.forEach(d => this.addProducer(new Producer(this, d)));
+    }
+
+    addProducer(arg){
+        this._producers.push(arg);
+        this._totalProduction += arg.production;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    get producers(){
+        return this._producers.slice();
+    }
+
+    get totalProduction(){
+        return this._totalProduction;
+    }
+
+    set totalProduction(arg){
+        this._totalProduction = arg;
+    }
+
+    get demand(){
+        return this._demand;
+    }
+
+    set demand(arg){
+        this._demand = parseInt(arg);
+    }
+
+    get price(){
+        return this._price;
+    }
+
+    set price(arg){
+        this._price = parseInt(arg);
+    }
+}
+
 class Producer{
     constructor(aProvince, data) {
         this._province = aProvince;
