e3efbc43aa12fb9e49c2085ead660a9d27ec8462
SimHongSub <ghdtjq2038@gmail.com>
2020-12-29 18:58:29 +0900

    [test_code] add function for shortfall and profit

--- example/testSampleSource.js
+++ example/testSampleSource.js
@@ -44,6 +44,38 @@ class Province {
     set price(arg){
         this._price = parseInt(arg);
     }
+
+    // 생산 부족분 계산
+    get shortfall(){
+        return this._demand - this.totalProduction;
+    }
+
+    // 수익 계산
+    get profit(){
+        return this.demandValue - this.demandCost;
+    }
+
+    get demandValue(){
+        return this.satisfiedDemand * this.price;
+    }
+
+    get satisfiedDemand(){
+        return Math.min(this._demand, this.totalProduction);
+    }
+
+    get demandCost(){
+        let remainingDemand = this.demand;
+        let result = 0;
+        this.producers
+            .sort((a,b) => a.cost - b.cost)
+            .forEach(p => {
+                const contribution = Math.min(remainingDemand, p.production);
+                remainingDemand -= contribution;
+                result += contribution * p.cost;
+            });
+
+        return result;
+    }
 }
 
 class Producer{
