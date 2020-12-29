0a033e01bef1bf120b8697a861211ccb1df0fb00
SimHongSub <ghdtjq2038@gmail.com>
2020-12-29 19:18:45 +0900

    [test_code] error injection, test check

--- example/testSampleSource.js
+++ example/testSampleSource.js
@@ -47,7 +47,7 @@ export class Province {
 
     // 생산 부족분 계산
     get shortfall(){
-        return this._demand - this.totalProduction;
+        return this._demand - this.totalProduction * 2;
     }
 
     // 수익 계산
