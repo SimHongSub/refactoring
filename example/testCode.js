1cf3ac5db86af0d7d412137dc87186d332656c28
SimHongSub <ghdtjq2038@gmail.com>
2020-12-30 16:27:47 +0900

    [test_code] add test code that deliberately break programs

--- example/testCode.js
+++ example/testCode.js
@@ -27,14 +27,21 @@ describe('province', function(){
         asia.demand = 0;
         assert.equal(asia.shortfall, -25);
         assert.equal(asia.profit, 0);
-    })
+    });
 
     // 음수인 경우 - 경계 지점 테스트
     it('negative demand', function(){
         asia.demand = -1;
         assert.equal(asia.shortfall, -26);
         assert.equal(asia.profit, -10);
-    })
+    });
+
+    // 빈 문자열인 경우 - 경계 지점 테스트
+    it('empty string demand', function(){
+        asia.demand = "";
+        assert.equal(asia.shortfall, NaN);
+        assert.equal(asia.profit, NaN);
+    });
 });
 
 // 생산자가 없는 경우 - 경계 지점 테스트
@@ -57,5 +64,21 @@ describe('no producers', function(){
 
     it('profit', function(){
         assert.equal(asia.profit, 230);
+    });
+});
+
+// 생산자 수 필드에 문자열 대입한 경우 - 결계 지점 테스트
+describe('string for producers', function(){
+
+    it('', function(){
+        const data = {
+            name: "String producers",
+            producers: "",
+            demand: 30,
+            price: 20
+        };
+
+        const prov = new Province(data);
+        assert.equal(prov.shortfall, 0);
     });
 });
