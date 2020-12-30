9fd8d8b30b233a3208489b67003ddcfa58ef62e5
SimHongSub <ghdtjq2038@gmail.com>
2020-12-30 16:13:14 +0900

    [test_code] add producer class production setter function test code

--- example/testCode.js
+++ example/testCode.js
@@ -2,13 +2,23 @@ import {Province, sampleProvinceData} from './testSampleSource.js'
 import * as assert from "assert";
 
 describe('province', function(){
+    let asia;
+    beforeEach(function(){
+        asia = new Province(sampleProvinceData());
+    });
+
     it('shortfall', function(){
-        const asia = new Province(sampleProvinceData());
         assert.equal(asia.shortfall, 5);
     });
 
     it('profit', function(){
-        const asia = new Province(sampleProvinceData());
         assert.equal(asia.profit, 230);
     });
+
+    // Producer production setter 테스트
+    it('change production', function(){
+        asia.producers[0].production = 20;
+        assert.equal(asia.shortfall, -6);
+        assert.equal(asia.profit, 292);
+    })
 });
