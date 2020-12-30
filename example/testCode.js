68dacf6182e2661492bac1f0a3a2ab3d5fa50964
SimHongSub <ghdtjq2038@gmail.com>
2020-12-30 16:20:14 +0900

    [test_code] add boundary point test code

--- example/testCode.js
+++ example/testCode.js
@@ -20,5 +20,42 @@ describe('province', function(){
         asia.producers[0].production = 20;
         assert.equal(asia.shortfall, -6);
         assert.equal(asia.profit, 292);
+    });
+
+    // 수요 0인 경우 - 경계 지점 테스트
+    it('zero demand', function(){
+        asia.demand = 0;
+        assert.equal(asia.shortfall, -25);
+        assert.equal(asia.profit, 0);
     })
+
+    // 음수인 경우 - 경계 지점 테스트
+    it('negative demand', function(){
+        asia.demand = -1;
+        assert.equal(asia.shortfall, -26);
+        assert.equal(asia.profit, -10);
+    })
+});
+
+// 생산자가 없는 경우 - 경계 지점 테스트
+describe('no producers', function(){
+    let noProducers;
+    beforeEach(function(){
+        const data = {
+            name: "No producers",
+            producers: [],
+            demand: 30,
+            price: 20
+        };
+
+        noProducers = new Province(data);
+    });
+
+    it('shortfall', function(){
+        assert.equal(asia.shortfall, 5);
+    });
+
+    it('profit', function(){
+        assert.equal(asia.profit, 230);
+    });
 });
