ee5bd3f630b1e348e47736c2ce3b6c376a277079
SimHongSub <ghdtjq2038@gmail.com>
2021-01-02 23:37:34 +0900

    [basic_refactoring] extract print details

--- example/basic/extractFunction/refactoringSource.js
+++ example/basic/extractFunction/refactoringSource.js
@@ -10,9 +10,7 @@ function printOwing(invoice){
     const today = Clock.today;
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`);
-    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+    printDetails();
 }
 
 // 배너 출력 함수 추출
@@ -20,4 +18,11 @@ function printBanner(){
     console.log("****************");
     console.log("**** 고객 채무 ****");
     console.log("****************");
+}
+
+// 세부 사항 출력 함수 추출
+function printDetails(){
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
