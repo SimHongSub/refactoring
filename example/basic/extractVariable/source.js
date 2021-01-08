3cc5e8b18beb485b69f9802a510580a8dcf5d68d
SimHongSub <ghdtjq2038@gmail.com>
2021-01-08 23:16:14 +0900

    [basic_refactoring] add extract variable example source in class

--- example/basic/extractVariable/source.js
+++ example/basic/extractVariable/source.js
@@ -3,3 +3,18 @@ function price(order){
         Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
         Math.min(order.quantity * order.itemPrice * 0.1, 100);
 }
+
+class Order{
+    constructor(aRecord) {
+        this._data = aRecord;
+    }
+
+    get quantity(){return this._data.quantity;}
+    get itemPrice(){return this._data.itemPrice;}
+
+    get price(){
+        return this.quantity * this.itemPrice -
+            Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
+            Math.min(this.quantity * this.itemPrice * 0.1, 100);
+    }
+}
