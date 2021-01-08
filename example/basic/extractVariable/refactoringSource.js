0f371cde49001669b3ebaba650ed4df1d366a84b
SimHongSub <ghdtjq2038@gmail.com>
2021-01-08 23:12:37 +0900

    [basic_refactoring] extract discount variable

--- example/basic/extractVariable/refactoringSource.js
+++ example/basic/extractVariable/refactoringSource.js
@@ -1,8 +1,8 @@
 function price(order){
     //가격(price) = 기본 가격 - 수량 할인 + 배송비
     const basePrice = order.quantity * order.itemPrice;
-
-    return basePrice -
-        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-        Math.min(order.quantity * order.itemPrice * 0.1, 100);
+    const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+    
+    return basePrice - quantityDiscount +
+        Math.min(basePrice * 0.1, 100);
 }
