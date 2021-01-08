ca77a18572d451dcd87c64453e2fed32f68417ab
SimHongSub <ghdtjq2038@gmail.com>
2021-01-08 23:20:31 +0900

    [basic_refactoring] extract as a method instead of a variable in class

--- example/basic/extractVariable/refactoringSource.js
+++ example/basic/extractVariable/refactoringSource.js
@@ -6,3 +6,21 @@ function price(order){
 
     return basePrice - quantityDiscount + shipping;
 }
+
+class Order{
+    constructor(aRecord) {
+        this._data = aRecord;
+    }
+
+    get quantity(){return this._data.quantity;}
+    get itemPrice(){return this._data.itemPrice;}
+
+    // 메서드로 추출
+    get basePrice(){return this.quantity * this.itemPrice;}
+    get quantityDiscount(){return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;}
+    get shipping(){return Math.min(this.basePrice * 0.1, 100);}
+
+    get price() {
+        return this.basePrice - this.quantityDiscount + this.shipping;
+    }
+}
