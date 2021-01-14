b706bfa46e9e14884aef411231aae1fcf5536f47
SimHongSub <ghdtjq2038@gmail.com>
2021-01-14 23:57:33 +0900

    [basic_refactoring] Class declaration representing scope

---
+++ example/basic/parameterObejct/refactoringSource.js
@@ -0,0 +1,43 @@
+const station = {
+    name: "ZB1",
+    readings: [
+        {
+            temp: 47,
+            time: "2016-11-10 09:10"
+        },
+        {
+            temp: 53,
+            time: "2016-11-10 09:20"
+        },
+        {
+            temp: 58,
+            time: "2016-11-10 09:10"
+        },
+        {
+            temp: 53,
+            time: "2016-11-10 09:10"
+        },
+        {
+            temp: 51,
+            time: "2016-11-10 09:10"
+        },
+    ]
+};
+
+// 정상 범위를 벗어난 측정값을 찾는 함수
+function readingsOutsideRange(station, min, max, range){
+    return station.readings
+        .filter(r => r.temp < min || r.temp > max);
+}
+
+// 범위 개념을 객체 하나로 묶어 표
+class NumberRange{
+    constructor(min, max) {
+        this._data = {min: min, max: max};
+    }
+    get min(){return this._data.min;}
+    get max(){return this._data.max;}
+}
+
+// 함수 호출문
+alerts = readingsOutsideRange(station, operatingPlan.temperatureFloor, operatingPlan.temperatureCeiling, null);
