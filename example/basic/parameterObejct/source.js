e11177a0e0746c1f1fae295872a6278008a2c256
SimHongSub <ghdtjq2038@gmail.com>
2021-01-14 23:52:30 +0900

    [basic_refactoring] modify example source

--- example/basic/parameterObejct/source.js
+++ example/basic/parameterObejct/source.js
@@ -24,7 +24,11 @@ const station = {
     ]
 };
 
+// 정상 범위를 벗어난 측정값을 찾는 함수
 function readingsOutsideRange(station, min, max){
     return station.readings
         .filter(r => r.temp < min || r.temp > max);
 }
+
+// 함수 호출문
+alerts = readingsOutsideRange(station, operatingPlan.temperatureFloor, operatingPlan.temperatureCeiling);
