52e2cceec772aa4931ca27dda11ab8ff5aa092c0
SimHongSub <ghdtjq2038@gmail.com>
2021-01-16 23:28:10 +0900

    [basic_refactoring] add function into class

--- example/basic/combineFunctions/refactoringSource.js
+++ example/basic/combineFunctions/refactoringSource.js
@@ -21,4 +21,22 @@ class Reading{
     get year(){
         return this._year;
     }
+
+    get calculateBaseCharge(){
+        return baseRate(this.month, this.year) * this.quantity;
+    }
 }
+
+// 클라이언트 1
+const aReading = acquireReading();
+const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+
+// 클라이언트 2
+const aReading = acquireReading();
+const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
+const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+
+// 클라이언트 3
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const basicCharge = aReading.calculateBaseCharge;
